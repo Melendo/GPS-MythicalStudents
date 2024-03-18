de340cab4c154c92bccd733a8648d46aeffb82d2
AlvaroDelCampoUCM <alcamp03@ucm.es>
2024-03-18 18:32:08 +0100

    session idUsuario

--- public/javascripts/comprarCromo.js
+++ public/javascripts/comprarCromo.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 
     $("#comprar").on('click', function() {
-        //var idUsuario = req.session.user.id;
-        //var idUsuario = req.session.user.ID;
         //var idSobre = #('#sobre').val();
         $.ajax({
             url: "/comprarCromo",
             type: "POST",
-            data: { idUsuario, idSobre }, // Debes enviar los datos en un objeto
+            data: { idSobre }, // Debes enviar los datos en un objeto
             success: function(response) {
                 if (response.success) {
                     alert(response.mensaje);
--- routes/comprarCromo.js
+++ routes/comprarCromo.js
@@ -5,13 +5,13 @@ const db = require('../connection/connection.js');
 
 
 router.post('/comprarCromo', (req, res) => {
-
+    var idUsuario = req.session.user.ID;
     db.getConnection(function (error, con) {
         const querySqlComprobarMonedas = 'SELECT MONEDAS FROM usuario WHERE ID = ?';
         const querySqlCosteSobre = 'SELECT COSTE FROM sobre WHERE ID = ?'
         const querySqlRestarMonedas = 'UPDATE usuario SET MONEDAS = ? WHERE ID = ?'
 
-        let { idUsuario, idSobre } = req.body;
+        let { idSobre } = req.body;
 
         con.query(querySqlComprobarMonedas, [idUsuario], (error, monedasResult) => {
             if (error) {
