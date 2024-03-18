e20b88a47d1c145c1760c8e37351f73c634cac97
AlvaroDelCampoUCM <alcamp03@ucm.es>
2024-03-18 18:41:11 +0100

    session albumes

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,32 @@ const db = require('../connection/connection.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  db.getConnection(function (error, con) {
-    const querySql = 'SELECT * FROM usuario WHERE id = 1';
+  //cuando se haga lo de los multiples usuarios:
+  //var user = req.session.user (que habra sido definido previamente en iniciar sesion)
+  //lo mismo con los albumes personales
 
-    con.query(querySql, [], (error, result) => {
+  db.getConnection(function (error, con) {
+    const querySqlUsuario = 'SELECT * FROM usuario WHERE ID = 1';
+    const querySqlAlbumes = 'SELECT * FROM album_personal WHERE ID_USU = 1';
+
+    con.query(querySqlUsuario, [], (error, usuarios) => {
       if (error) {
         con.release();
         throw error;
       }
       con.release();
-      req.session.user = result[0];
-      res.render('index', { user: req.session.user, title: "Pagina Principal" });
+      req.session.user = usuarios[0];
+
+      con.query(querySqlAlbumes, [], (error, albumes) => {
+        if (error) {
+          con.release();
+          throw error;
+        }
+        con.release();
+        req.session.albumes = albumes;
+
+        res.render('index', { user: req.session.user, albumes: req.session.albumes, title: "Pagina Principal" });
+      });
     });
   });
 });
