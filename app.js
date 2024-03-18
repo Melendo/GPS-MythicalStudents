a1f0bb0e73a2aeb97875111e45ce48b9bd8f41c7
IvanAlcCam <ivanalca@ucm.es>
2024-03-18 01:04:26 +0100

    added sessions app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+const mysqlSession = require("express-mysql-session");
+
+const MySQLStore = mysqlSession(session);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var crearColeccionRouter = require('./routes/crearColeccion');
