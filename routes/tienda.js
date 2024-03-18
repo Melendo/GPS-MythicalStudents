eb18eae7e8008e933a2c21ef6d7d64c712265340
IvanAlcCam <ivanalca@ucm.es>
2024-03-18 01:15:25 +0100

    agregado router tienda

---
+++ routes/tienda.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+const db = require('../connection/connection.js');
+
+
+router.get('/', function (req, res, next) {
+
+    db.getConnection(function (error, con) {
+        if (error) {
+            con.release();
+            throw error;
+        }
+        
+        const sqlAlbumes = "SELECT * FROM sobre, album_personal WHERE sobre.ALBUM = album_personal.ID_ALBUM AND album_personal.ID_USU = ?"
+        con.query(sqlAlbumes, [req.sessions.user.id], (error, result) => {
+            if (error) {
+                con.release();
+                throw error;
+            }
+            con.release();
+            res.render('tienda', { title: 'Tienda', sobres : result });
+        });
+    });
+
+});
+
+module.exports = router;
