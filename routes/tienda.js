18d13e4bbbcfa9c6fdf914cf30189005a79a352e
IvanAlcCam <ivanalca@ucm.es>
2024-03-20 17:26:49 +0100

    Mostrar imagenes de sobres en la tienda

--- routes/tienda.js
+++ routes/tienda.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 const db = require('../connection/connection.js');
 
 
@@ -18,7 +23,29 @@ router.get('/', function (req, res, next) {
                 throw error;
             }
             con.release();
-            res.render('tienda', { title: 'Tienda', sobres : result });
+
+            res.render('tienda', { title: 'Tienda', sobres: result });
+        });
+    });
+
+});
+
+router.get('/imagen:id', function (req, res, next) {
+    db.getConnection(function (error, con) {
+        if (error) {
+            con.release();
+            throw error;
+        }
+        const sqlAlbumes = "SELECT IMAGEN FROM sobre WHERE ID = ?";
+        con.query(sqlAlbumes, [req.params.id], (error, result) => {
+            if (error) {
+                con.release();
+                throw error;
+            }
+            con.release();
+            
+            // Envía la imagen como respuesta al cliente
+            res.send(result[0].IMAGEN);
         });
     });
 
