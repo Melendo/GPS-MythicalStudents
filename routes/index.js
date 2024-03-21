1c86aa13e2c40e59f50d0b962709669adbb145a1
pedrocuadracarri <pedrocuadracarri@gmail.com>
2024-03-21 10:47:19 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('snowflake', { title: 'Express' });
+  res.render('tienda', { title: 'Express' });
 });
 
 module.exports = router;
