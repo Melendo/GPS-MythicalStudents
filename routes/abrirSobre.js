2e7ca161584a0e693fd919f884ebddb3ae0762f8
valeriacorinapl <valeriacorinapl@gmail.com>
2024-03-22 15:23:04 +0100

    Pantalla apertura

--- routes/abrirSobre.js
+++ routes/abrirSobre.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var btnAbrirSobre = document.getElementById('btnAbrirSobre');
     if (btnAbrirSobre) {
         btnAbrirSobre.addEventListener('click', function() {
-            // Redireccionar al usuario a la página del sobre
-            window.location.href = '/pagina-del-sobre';
+            // ACCION DE ABRIR SOBRE
         });
     }
 });
