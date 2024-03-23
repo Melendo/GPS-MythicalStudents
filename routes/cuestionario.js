74f05be96db60ab484e2dcf0229ba819aaefb63f
IvanAlcCam <ivanalca@ucm.es>
2024-03-23 14:07:07 +0100

    Router reclamar recompensa empezado

--- routes/cuestionario.js
+++ routes/cuestionario.js
@@ -3,14 +3,25 @@ var router = express.Router();
 const db = require('../connection/connection.js');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 
     res.render("finalCuestionario");
 });
 
-router.get("/reclamar", function(req, res) {
+router.get("/reclamar:monedas", function (req, res) {
+    var recompensa = req.params.monedas + req.session.user.MONEDAS;
+    db.getConnection(function (error, con) {
+        const querySqlMonedas = 'UPDATE usuario SET MONEDAS = ? WHERE ID = ?';
+        con.query(querySqlMonedas, [recompensa, req.session.USER.id], (error, result) => {
+            if (error) {
+                con.release();
+                throw error;
+            }
+            con.release();
 
-    res.redirect("index");
+            res.render('index', { user: req.session.user, albumes: req.session.albumes, title: "Pagina Principal", monedas: req.session.user.MONEDAS });
+        });
+    });
 });
 
 
