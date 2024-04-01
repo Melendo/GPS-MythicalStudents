3576733950d3e000a97f41207350a6752a4d63ba
pedrocuadracarri <pedrocuadracarri@gmail.com>
2024-04-01 21:19:10 +0200

    reclamarMonedas

--- routes/cuestionario.js
+++ routes/cuestionario.js
@@ -2,25 +2,38 @@ var express = require('express');
 var router = express.Router();
 const db = require('../connection/connection.js');
 
-/* GET home page. */
-router.get('/', function (req, res) {
 
-    res.render("finalCuestionario");
-});
-
-router.get("/reclamar:monedas", function (req, res) {
-    var recompensa = req.params.monedas + req.session.user.MONEDAS;
-    db.getConnection(function (error, con) {
-        const querySqlMonedas = 'UPDATE usuario SET MONEDAS = ? WHERE ID = ?';
-        con.query(querySqlMonedas, [recompensa, req.session.USER.id], (error, result) => {
-            if (error) {
-                con.release();
-                throw error;
-            }
+// Función para actualizar el número total de monedas
+function actualizarMonedas(idUsuario, nuevasMonedas, callback) {
+    const sqlActualizarMonedas = "UPDATE usuarios SET MONEDAS = MONEDAS + ? WHERE ID = ?";
+    db.getConnection(function(error, con) {
+        if (error) {
             con.release();
-
-            res.render('index', { user: req.session.user, albumes: req.session.albumes, title: "Pagina Principal", monedas: req.session.user.MONEDAS });
+            throw error;
+        }
+        con.query(sqlActualizarMonedas, [nuevasMonedas, idUsuario], (error, result) => {
+            con.release();
+            if (error) {
+                return callback(error);
+            }
+            callback();
         });
+    });
+}
+
+// Manejador de la ruta
+router.get('/:monedas', function(req, res) {
+    const idUsuario = req.session.user.ID;
+    const nuevasMonedas = req.params.monedas;
+
+    actualizarMonedas(idUsuario, nuevasMonedas, (error) => {
+        if (error) {
+            con.release();
+            throw error;
+        } 
+        else {
+            res.json({ mensaje: "Número de monedas actualizado correctamente" });
+        }
     });
 });
 
