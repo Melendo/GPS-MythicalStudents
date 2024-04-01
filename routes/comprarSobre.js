0fe3515d3e4a5123876cde1e485c0e522200bb3e
AlvaroDelCampoUCM <alcamp03@ucm.es>
2024-04-01 21:14:12 +0200

    Cambios insertar cromos

--- routes/comprarSobre.js
+++ routes/comprarSobre.js
@@ -4,90 +4,38 @@ var router = express.Router();
 const db = require('../connection/connection.js');
 
 
+// Función principal para manejar la ruta POST
 router.post('/:id', (req, res) => {
-    
-    var idUsuario = req.session.user.ID;
-    var monUsuario = req.session.user.MONEDAS;
-    var idSobre = req.params.id;
+    const idUsuario = req.session.user.ID;
+    const monUsuario = req.session.user.MONEDAS;
+    const idSobre = req.params.id;
 
-    db.getConnection(function (error, con) {
-        const querySqlSobre = 'SELECT * FROM sobre WHERE ID = ?';
-        const querySqlComprobarAlbum = 'SELECT * FROM album_personal WHERE ID_USU = ? AND ID_ALBUM = ?';
-        const querySqlRestarMonedas = 'UPDATE usuario SET MONEDAS = ? WHERE ID = ?';
-        const querySqlCromos = 'SELECT COUNT(*) FROM cromos WHERE ALBUM = ?';
-        const querySqlCromosEnCromosPersonal = 'SELECT * FROM cromos_personal WHERE ID_USU = ? AND ID_CROMO = ?';
-        const querySqlCromoRepetido = 'UPDATE cromos_personal SET CANTIDAD = CANTIDAD + 1 WHERE ID_USU = ? AND ID_CROMO = ?';
-        const querySqlCromoNuevo = 'INSERT INTO cromos_personal (ID_USU, ID_CROMO, CANTIDAD) VALUES (?, ?, 1)';
+    db.getConnection((error, con) => {
+        if (error) {
+            con.release();
+            throw error;
+        }
 
-        con.query(querySqlSobre, [idSobre], (error, sobresResult) => {
-            if (error) {
-                con.release();
-                throw error;
-            }
-
-            con.query(querySqlComprobarAlbum, [idUsuario, sobresResult[0].ALBUM], (error, albumesResult) => {
-                if (error) {
-                    con.release();
-                    throw error;
-                }
-    
+        obtenerSobre(con, idSobre, (sobresResult) => {
+            comprobarAlbum(con, idUsuario, sobresResult, (albumesResult) => {
                 if (albumesResult.length <= 0) {
-                    res.json({ success: false, mensaje: "No tienes el album al que pertenece el sobre" });
-                }
-    
+                    res.json({ success: false, mensajeError: "No tienes el álbum al que pertenece el sobre" });
+                } 
                 else {
-                    if (monUsuario < sobresResult[0].PRECIO) {
-                        res.json({ success: false, mensaje: "Monedas insuficientes" });
-                    }
+                    if (monUsuario < sobresResult.PRECIO) {
+                        res.json({ success: false, mensajeError: "Monedas insuficientes" });
+                    } 
                     else {
-                        var resta = monUsuario - sobresResult[0].PRECIO;
-                        con.query(querySqlRestarMonedas, [resta, idUsuario], (error, restaResult) => {
-                            if (error) {
-                                con.release();
-                                throw error;
-                            }
+                        const resta = monUsuario - sobresResult.PRECIO;
+                        restarMonedas(con, resta, idUsuario, () => {
                             req.session.user.MONEDAS = resta;
-                            
-                            con.query(querySqlCromos, [sobresResult[0].ALBUM], (error, totalCromos) => {
-                                if (error) {
-                                    con.release();
-                                    throw error;
-                                }
-                                cantidadCromos = totalCromos[0]['COUNT(*)'];
-                                
-                                // Generar números aleatorios y continuar una vez que estén listos
-                                generarNumerosAleatorios(cantidadCromos, sobresResult[0].NUM_CROMOS, (numerosAleatorios) => {
-                                    console.log("Array: " + numerosAleatorios);
-                                    numerosAleatorios.forEach(numero => {
-                                        console.log(numero);
-                                        con.query(querySqlCromosEnCromosPersonal, [idUsuario, numero], (error, cromosResults) => {
-                                            if (error) {
-                                                con.release();
-                                                throw error;
-                                            }
-                                
-                                            if (cromosResults.length > 0) {
-                                                con.query(querySqlCromoRepetido, [idUsuario, numero], (error, results) => {
-                                                    if (error) {
-                                                        con.release();
-                                                        throw error;
-                                                    }
-                                                });
-                                            } 
-                                            else {
-                                                con.query(querySqlCromoNuevo, [idUsuario, numero], (error, results) => {
-                                                    if (error) {
-                                                        con.release();
-                                                        throw error;
-                                                    }
-                                                });
-                                            }
-                                        });
+                            abrirSobre(con, sobresResult, (totalCromos) => {
+                                generarNumerosAleatorios(totalCromos['COUNT(*)'], sobresResult.NUM_CROMOS, (numerosAleatorios) => {
+                                    procesarCromos(con, idUsuario, numerosAleatorios, () => {
+                                        res.json({ success: true, mensaje: "Compra realizada correctamente", album: sobresResult.ALBUM, numeros: numerosAleatorios });
                                     });
-
-                                    res.json({ success: true, mensaje: "Compra realizada correctamente" });
                                 });
-                            });                           
+                            });
                         });
                     }
                 }
@@ -96,10 +44,111 @@ router.post('/:id', (req, res) => {
     }); 
 });
 
+// Función para obtener la información del sobre
+function obtenerSobre(con, idSobre, callback) {
+    const querySqlSobre = 'SELECT * FROM sobre WHERE ID = ?';
+    con.query(querySqlSobre, [idSobre], (error, sobresResult) => {
+        if (error) {
+            con.release();
+            throw error;
+        }
+        callback(sobresResult[0]);
+    });
+}
+
+// Función para comprobar si el usuario tiene el álbum al que pertenece el sobre
+function comprobarAlbum(con, idUsuario, sobresResult, callback) {
+    const querySqlComprobarAlbum = 'SELECT * FROM album_personal WHERE ID_USU = ? AND ID_ALBUM = ?';
+    con.query(querySqlComprobarAlbum, [idUsuario, sobresResult.ALBUM], (error, albumesResult) => {
+        if (error) {
+            con.release();
+            throw error;
+        }
+        callback(albumesResult);
+    });
+}
+
+// Función para restar las monedas del usuario
+function restarMonedas(con, resta, idUsuario, callback) {
+    const querySqlRestarMonedas = 'UPDATE usuario SET MONEDAS = ? WHERE ID = ?';
+    con.query(querySqlRestarMonedas, [resta, idUsuario], (error, restaResult) => {
+        if (error) {
+            con.release();
+            throw error;
+        }
+        callback();
+    });
+}
+
+// Función para abrir el sobre y obtener el número total de cromos en el álbum
+function abrirSobre(con, sobresResult, callback) {
+    const querySqlCromos = 'SELECT COUNT(*) FROM cromos WHERE ALBUM = ?';
+    con.query(querySqlCromos, [sobresResult.ALBUM], (error, totalCromos) => {
+        if (error) {
+            con.release();
+            throw error;
+        }
+        callback(totalCromos[0]);
+    });
+}
+
+// Función para procesar los cromos obtenidos del sobre
+function procesarCromos(con, idUsuario, numerosAleatorios, callback) {
+    const querySqlCromosEnCromosPersonal = 'SELECT * FROM cromos_personal WHERE ID_CROMO = ?';
+    const querySqlCromoRepetido = 'UPDATE cromos_personal SET CANTIDAD = CANTIDAD + 1 WHERE ID_USU = ? AND ID_CROMO = ?';
+    const querySqlCromoNuevo = 'INSERT INTO cromos_personal (ID_USU, ID_CROMO, CANTIDAD) VALUES (?, ?, 1)';
+    
+    // Función auxiliar para procesar un cromo
+    function procesarCromo(numero, index) {
+        return new Promise((resolve, reject) => {
+            con.query(querySqlCromosEnCromosPersonal, [numero], (error, results) => {
+                if (error) {
+                    reject(error);
+                } 
+                else {
+                    if (results.length > 0) {
+                        con.query(querySqlCromoRepetido, [idUsuario, numero], (error) => {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                resolve();
+                            }
+                        });
+                    } 
+                    else {
+                        con.query(querySqlCromoNuevo, [idUsuario, numero], (error) => {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                resolve();
+                            }
+                        });
+                    }
+                }
+            });
+        });
+    }
+
+    // Utilizando reduce para procesar las promesas secuencialmente
+    numerosAleatorios.reduce((promiseChain, numero, index) => {
+        return promiseChain.then(() => {
+            return procesarCromo(numero, index);
+        });
+    }, Promise.resolve())
+    .then(() => {
+        callback();
+    })
+    .catch((error) => {
+        con.release();
+        throw error;
+    });
+}
+
+// Función para generar números aleatorios
 function generarNumerosAleatorios(max, num, callback) {
     const numerosAleatorios = [];
     while (numerosAleatorios.length < num) {
-        const numeroAleatorio = Math.floor(Math.random() * max) + 1;
+        const numeroAleatorio = Math.floor(Math.random() * (max - 1 + 1)) + 1;
         numerosAleatorios.push(numeroAleatorio);
     }
     callback(numerosAleatorios);
