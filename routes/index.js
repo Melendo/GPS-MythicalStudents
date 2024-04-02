fec564e97955fa0c0feb239347c93ebba6475080
IvanAlcCam <ivanalca@ucm.es>
2024-04-03 01:34:51 +0200

    Develop a main... forzado un poco :3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../connection/connection.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('crearColeccion', { title: 'Express' });
+router.get('/', function(req, res) {
+  //cuando se haga lo de los multiples usuarios:
+  //var user = req.session.user (que habra sido definido previamente en iniciar sesion)
+  //lo mismo con los albumes personales
+
+  db.getConnection(function (error, con) {
+    const querySqlUsuario = 'SELECT * FROM usuario WHERE ID = 1';
+    const querySqlAlbumes = 'SELECT * FROM album_personal WHERE ID_USU = 1';
+
+    con.query(querySqlUsuario, [], (error, usuarios) => {
+      if (error) {
+        con.release();
+        throw error;
+      }
+      con.release();
+      req.session.user = usuarios[0];
+
+      con.query(querySqlAlbumes, [], (error, albumes) => {
+        if (error) {
+          con.release();
+          throw error;
+        }
+        
+        req.session.albumes = albumes;
+
+        res.render('index', { user: req.session.user, title: "Pagina Principal", monedas:req.session.user.MONEDAS });
+      });
+    });
+
+
+   // res.render('index', { user: global.user, title: "Pagina Principal" });
+
+  });
 });
 
 module.exports = router;
