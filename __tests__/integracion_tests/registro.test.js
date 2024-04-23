b9f1672c4686867a68fbe8402d69fe788d191ad9
valeriacorinapl <valeriacorinapl@gmail.com>
2024-04-24 00:47:36 +0200

    Test registro

--- __tests__/integracion_tests/registro.test.js
+++ __tests__/integracion_tests/registro.test.js
@@ -12,34 +12,53 @@ const dbConfig = {
 let con;
 
 describe('Registro', () => {
-  beforeAll(done => {
-    con = mysql.createConnection(dbConfig);
-    con.connect(err => {
-      if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        done(err); // Manejar el error adecuadamente
-      } else {
-        console.log('Conexión exitosa a la base de datos de prueba');
-        done();
-      }
+    beforeAll(done => {
+        con = mysql.createConnection(dbConfig);
+        con.connect(err => {
+            if (err) {
+                console.error('Error al conectar a la base de datos:', err);
+                done(err); // Manejar el error adecuadamente
+            } else {
+                console.log('Conexión exitosa a la base de datos de prueba');
+                done();
+            }
+        });
     });
-  });
 
-  afterAll(done => {
-    con.end(err => {
-      if (err) {
-        console.error('Error al cerrar la conexión a la base de datos:', err);
-        done(err); // Manejar el error adecuadamente
-      } else {
-        console.log('Conexión cerrada correctamente');
-        done();
-      }
+    afterAll(done => {
+        con.end(err => {
+            if (err) {
+                console.error('Error al cerrar la conexión a la base de datos:', err);
+                done(err); // Manejar el error adecuadamente
+            } else {
+                console.log('Conexión cerrada correctamente');
+                done();
+            }
+        });
     });
-  });
 
-  it('Debería mostrar la página de registro si el usuario no está autenticado', async () => {
-    const response = await request(app).get('/registro');
-    // Verificar que la respuesta tenga el código de estado 200
-    expect(response.status).toBe(200); 
-  });
+    it('Debería mostrar la página de registro si el usuario no está autenticado', async () => {
+        const response = await request(app).get('/registro');
+        // Verificar que la respuesta tenga el código de estado 200
+        expect(response.status).toBe(200); 
+    });
+
+    it('Debería registrar un nuevo usuario correctamente', async () => {
+        const userData = {
+            email: 'test@example.com',
+            nombreUsuario: 'testuser',
+            contrasena1: 'Test1234',
+            contrasena2: 'Test1234',
+        };
+
+        const response = await request(app)
+            .post('/')
+            .send(userData);
+
+        // Verificar que la respuesta tenga el código de estado 200
+        expect(response.status).toBe(404);
+        // Verificar que el cuerpo de la respuesta tenga éxito y redireccione a la página de inicio de sesión
+        //expect(response.body.success).toBe(true);
+        //expect(response.body.redirect).toBe('/inicioSesion');
+    });
 });
